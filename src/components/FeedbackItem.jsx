a6508b7d4b370f1c6e3105066a1681dfa93aa283
Wahyu Pratama <wahyulucky28@gmail.com>
2022-09-14 21:03:43 +0700

    initial project

---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,10 @@
+function FeedbackItem() {
+  return (
+    <div className="card">
+      <div className="num-display">10</div>
+      <div className="text-display">This is an example of a feedback item</div>
+    </div>
+  );
+}
+
+export default FeedbackItem;
